d50b2d0a32bc2d3051d9d9195ca477600d910497
zapatistas <30812664+zapatistas@users.noreply.github.com>
2018-10-11 13:17:47 +0300

    !!

--- BDOBossTimer/app/src/main/java/com/penstack/dbobosstimer/Settings.java
+++ BDOBossTimer/app/src/main/java/com/penstack/dbobosstimer/Settings.java
@@ -1,13 +1,23 @@
 package com.penstack.dbobosstimer;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.view.View;
+import android.widget.RadioGroup;
 
-public class Settings extends  MainActivity {
+public class Settings extends AppCompatActivity {
+    RadioGroup radioGroup;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
 
+        radioGroup = (RadioGroup) findViewById(R.id.ServerGroup);
 
     }
 }
+
+
+
