4e2abd9cb2faf2113d043d5f5d40b0b04d456644
zapatistas <30812664+zapatistas@users.noreply.github.com>
2018-10-16 21:44:29 +0300

    checkbox
    save checkbox notification prefs

--- BDOBossTimer/app/src/main/java/com/penstack/dbobosstimer/MainActivity.java
+++ BDOBossTimer/app/src/main/java/com/penstack/dbobosstimer/MainActivity.java
@@ -1,8 +1,12 @@
 package com.penstack.dbobosstimer;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.CountDownTimer;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -21,7 +25,9 @@ import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Locale;
+import java.util.Set;
 import java.util.TimeZone;
 
 import static java.util.TimeZone.getTimeZone;
@@ -54,13 +60,24 @@ public long countdown,day;
     final String PREFS_NAME = "BDO_TIMER_PREFS";
     final String PREF_SERVER_CONSTANT = "0";
     final int DOESNT_EXIST = -1;
+    final ArrayList<String> NOTIFY_BOSS=new ArrayList<>();
+     Set<String> BossNotify=new HashSet<String>();
+    final String PREF_NOTIFY="";
     SharedPreferences prefs ;
-
+    public String CHANNEL_ID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*createNotificationChannel();
 
+            CHANNEL_ID="Boss channel";
+            NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                    .setSmallIcon(R.mipmap.ic_launcher_round)
+                    .setContentTitle("BOSS SPAWNING")
+                    .setContentText("Boss will spawn in 15mins!!")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+*/
         intentSettings = new Intent(MainActivity.this, Settings.class);
         intentFirstTime = new Intent(MainActivity.this, firstTimeUseActivity.class);
 
@@ -368,6 +385,28 @@ public long countdown,day;
         }else if(currentServerSelection == 2){
             ServerSelection(BossDayNAList, "-7");
         }
+
+
+
+           BossNotify=prefs.getStringSet(PREF_NOTIFY,null);
+
     }
 
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "bossTimer";
+            String description = "Notification for Boss spawning" ;
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            String CHANNEL_ID="Boss Channel";
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
 }
--- BDOBossTimer/app/src/main/java/com/penstack/dbobosstimer/Settings.java
+++ BDOBossTimer/app/src/main/java/com/penstack/dbobosstimer/Settings.java
@@ -4,8 +4,13 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.view.View;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Settings extends AppCompatActivity {
 
@@ -15,6 +20,9 @@ public class Settings extends AppCompatActivity {
     final int EUSERVER_CONSTANT = 1;
     final int NASERVER_CONSTANT = 2;
     final String PREF_SERVER_CONSTANT = "0";
+    final ArrayList<String> NOTIFY_BOSS=new ArrayList<>();
+    final Set<String> BossNotify=new HashSet<String>();
+    final String PREF_NOTIFY="";
     SharedPreferences prefs;
 
    @Override
@@ -42,6 +50,65 @@ public class Settings extends AppCompatActivity {
                     prefs.edit().putInt(PREF_SERVER_CONSTANT, NASERVER_CONSTANT).apply();
                     break;
         }
+    }
+
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+
+
+        switch(view.getId()) {
+            case R.id.checkKaranda:
+                if (checked) {
+                    NOTIFY_BOSS.add("Karanda");//prefs.edit().putStringSet("Karanda",BossNotify).apply();
+                } else
+                    NOTIFY_BOSS.add("");
+                break;
+            case R.id.checkKutum:
+                if (checked) {
+
+                    NOTIFY_BOSS.add("Kutum");// prefs.edit().putStringSet("Kutum",BossNotify).apply();
+                } else
+                    NOTIFY_BOSS.add("");
+                break;
+            case R.id.checkKzarka:
+                if (checked) {
+
+                    NOTIFY_BOSS.add("Kzarka");//prefs.edit().putStringSet("Kzarka",BossNotify).apply();
+                } else
+                    NOTIFY_BOSS.add("");
+                break;
+            case R.id.checkNouver:
+                if (checked) {
+
+                    NOTIFY_BOSS.add("Nouver");//prefs.edit().putStringSet("Nouver",BossNotify).apply();
+                } else
+                    NOTIFY_BOSS.add("");
+                break;
+            case R.id.checkQuint:
+                if (checked) {
+
+                    NOTIFY_BOSS.add("Quint");//prefs.edit().putStringSet("Quint",BossNotify).apply();
+                } else
+                    NOTIFY_BOSS.add("");
+                break;
+            case R.id.checkVell:
+                if (checked) {
+
+                    NOTIFY_BOSS.add("Vell");// prefs.edit().putString(NOTIFY_BOSS.get(5),"Vell").apply();
+                } else
+                    NOTIFY_BOSS.add("");
+                break;
+            case R.id.checkOffin:
+                if (checked) {
+
+                    NOTIFY_BOSS.add("Offin");// prefs.edit().putString(NOTIFY_BOSS.get(6),"Offin").apply();
+                } else
+                    NOTIFY_BOSS.add("");
+                break;
+        }
+         BossNotify.addAll(NOTIFY_BOSS);
+        prefs.edit().putStringSet(PREF_NOTIFY,BossNotify).apply();
     }
 }
 
