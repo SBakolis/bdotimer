ac166776f013f1f1c26eb86c503095b9cbf92c55
zapatistas <30812664+zapatistas@users.noreply.github.com>
2018-11-05 02:15:00 +0200

    load screen
    implemented a load screen

--- BDOBossTimer/app/src/main/java/com/penstack/dbobosstimer/MainActivity.java
+++ BDOBossTimer/app/src/main/java/com/penstack/dbobosstimer/MainActivity.java
@@ -1,16 +1,15 @@
 package com.penstack.dbobosstimer;
 
-import android.app.AlarmManager;
-import android.app.AlertDialog;
-import android.app.Notification;
+
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
+
+
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
-import android.os.CountDownTimer;
+
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -20,15 +19,15 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.RadioButton;
+
+
 import android.widget.TextView;
 
 import java.sql.Timestamp;
 
 import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
-import java.time.ZoneId;
+
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -37,7 +36,7 @@ import java.util.Locale;
 import java.util.Set;
 import java.util.TimeZone;
 
-import static android.support.v4.os.LocaleListCompat.create;
+
 import static java.util.TimeZone.getTimeZone;
 
 
@@ -80,6 +79,7 @@ public long countdown,day;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
 
@@ -241,7 +241,6 @@ public long countdown,day;
 
         Preferences();
         createNotificationChannel();
-
 
 
     }
